c772a746fa8841a9004b4ddfd922a0577508a5cb
jakubsvec001 <jakubsvec001@gmail.com>
2019-12-30 21:00:39 -0800

    COMPLETE implentation of AJAX request, start on integrating live data with app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,37 +2,47 @@ import VideoList from "./VideoList.js";
 import VideoPlayer from "./VideoPlayer.js";
 import Search from "./Search.js";
 import exampleVideoData from "../data/exampleVideoData.js";
+import searchYouTube from "../lib/searchYouTube.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: exampleVideoData,
-      selected: false,
-      selectedVideo: exampleVideoData[0]
+      videos: [],
+      selected: {}
     };
   }
 
-  videoSelectHandler() {
+  videoSelectHandler(video) {
     this.setState({
-      selected: !this.state.selected
+      selected: video
     });
   }
 
-  updateSelectedVideo() {
-    const selected = this.state.videos.filter(video => video.state.selected);
-    // console.log(selected)
+  componentDidMount() {
+    //make initial request
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: "react",
+      max: 5
+    };
+
+    searchYouTube(options, videosArray =>
+      this.setState({
+        videos: videosArray,
+        selected: videosArray[0]
+      })
+    );
   }
 
   render() {
     return (
       <div>
         <Search />
-        <VideoPlayer video={this.state.selectedVideo} />
+        <VideoPlayer video={this.state.selected} />
         <VideoList
           videos={this.state.videos}
           handler={this.videoSelectHandler.bind(this)}
-          updateSelectedVideo={this.updateSelectedVideo.bind(this)}
         />
       </div>
     );
